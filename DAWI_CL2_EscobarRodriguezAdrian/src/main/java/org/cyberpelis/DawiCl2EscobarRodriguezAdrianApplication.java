0bccdc60b86cb9586e7bea65f1192e5292e1904a
Adrianzzziny <adri-escobar@hotmail.com>
2022-05-25 20:19:25 -0500

    Primer Commit

---
+++ DAWI_CL2_EscobarRodriguezAdrian/src/main/java/org/cyberpelis/DawiCl2EscobarRodriguezAdrianApplication.java
@@ -0,0 +1,13 @@
+package org.cyberpelis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiCl2EscobarRodriguezAdrianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiCl2EscobarRodriguezAdrianApplication.class, args);
+	}
+
+}
